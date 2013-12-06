60f26afed1f7547d77768e8547c19c13788595d1
tobaby <tobabyzb@gmail.com>
2013-12-07 01:40:16 +0900

    フォルダ初期化バッチ

---
+++ tools/init_forder.bat
@@ -0,0 +1,7 @@
+mkdir ..\data\cache
+mkdir ..\data\Smarty\templates_c
+mkdir ..\data\Smarty\templates_c\admin
+mkdir ..\data\Smarty\templates_c\default
+mkdir ..\data\Smarty\templates_c\mobile
+mkdir ..\data\Smarty\templates_c\sphone
+mkdir ..\data\logs
