d0edba9d633c8581b8536888b057c7bbb4551a92
Min Si <msi@il.is.s.u-tokyo.ac.jp>
2014-04-15 14:52:33 +0900

    CSVダウンロードの時、すべてのcsv項目を""で囲む

--- data/class_extends/helper_extends/SC_Helper_CSV_Ex.php
+++ data/class_extends/helper_extends/SC_Helper_CSV_Ex.php
@@ -34,4 +34,45 @@ require_once CLASS_REALDIR . 'helper/SC_Helper_CSV.php';
  * @version $Id:SC_Helper_DB_Ex.php 15532 2007-08-31 14:39:46Z nanasess $
  */
 class SC_Helper_CSV_Ex extends SC_Helper_CSV {
+
+    /**
+     * 1次元配列を1行のCSVとして返す
+     * 参考: http://jp.php.net/fputcsv
+     * 
+     * @param array $fields データ1次元配列
+     * @param string $delimiter
+     * @param string $enclosure
+     * @param string $arrayDelimiter
+     * @return string 結果行
+     */
+    function sfArrayToCsv($fields, $delimiter = ',', $enclosure = '"', $arrayDelimiter = '|') {
+        if (strlen($delimiter) != 1) {
+            trigger_error('delimiter must be a single character', E_USER_WARNING);
+            return '';
+        }
+        
+        if (strlen($enclosure) < 1) {
+            trigger_error('enclosure must be a single character', E_USER_WARNING);
+            return '';
+        }
+        
+        foreach ( $fields as $key => $value ) {
+            $field = & $fields[$key];
+            
+            // 配列を「|」区切りの文字列に変換する
+            if (is_array($field)) {
+                $field = implode($arrayDelimiter, $field);
+            }
+            
+//             /* enclose a field that contains a delimiter, an enclosure character, or a newline */
+//             if (is_string($field) && preg_match('/[' . preg_quote($delimiter) . preg_quote($enclosure) . '\\s]/', $field)) {
+//                 $field = $enclosure . preg_replace('/' . preg_quote($enclosure) . '/', $enclosure . $enclosure, $field) . $enclosure;
+//             }
+            // 顧客様が手動で特殊コードを入れるから、すべての項目にenclosureを追加する
+            $field = $enclosure . preg_replace('/' . preg_quote($enclosure) . '/', $enclosure . $enclosure, $field) . $enclosure;
+        }
+        
+        return implode($delimiter, $fields);
+    }
+    
 }
